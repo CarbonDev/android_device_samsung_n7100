87651e25061f0e99a76776295914ae0d8af16223
Utkarsh Gupta <utkarsh.eminem@gmail.com>
2014-10-17 13:01:54 +0530

    NHI: Fingerprint
    Change-Id: I88cbd9d39bbc1b57fe173ee40c35cf16a3ec1203

--- carbon.mk
+++ carbon.mk
@@ -17,5 +17,5 @@ PRODUCT_MANUFACTURER := samsung
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="t03gxx-user 4.3 JSS15J N7100XXUENB2 release-keys" \
-    BUILD_FINGERPRINT="samsung/t03gxx/t03g:4.3/JSS15J/N7100XXUENB2:user/release-keys"
+    PRIVATE_BUILD_DESC="t03gxx-user 4.4.2 KOT49H N7100XXUFNI1 release-keys" \
+    BUILD_FINGERPRINT="samsung/t03gxx/t03g:4.4.2/KOT49H/N7100XXUFNI1:user/release-keys"
